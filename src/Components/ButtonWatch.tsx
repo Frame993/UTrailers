079f6d015445d7e34a1859db048749d6ab2fc93c
Yordani <yalvarezb93@gmail.com>
2024-04-02 22:35:23 +0300

    Add padding y to ButtonWatch

--- src/Components/ButtonWatch.tsx
+++ src/Components/ButtonWatch.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 export default function ButtonWatch({ text, icon }: Props) {
   return (
-    <button className="btn flex flex-row items-center gap-2 bg-[#E50000] w-fit px-6 py-3 rounded-lg uppercase font-bold">
+    <button className="btn flex flex-row items-center gap-2 bg-[#E50000] w-fit px-6 py-4 rounded-lg uppercase font-bold">
       <img src={icon} alt="icon" />
       {text}
     </button>
