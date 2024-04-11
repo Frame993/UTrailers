187ec5bf352ef101ff9dcc5eef22d64a7bd723ef
Yordani <yalvarezb93@gmail.com>
2024-04-11 19:01:26 +0300

    Add wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,37 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Details from './pages/Details.tsx';
+import {
+  Routes,
+  Route,
+  BrowserRouter as Router,
+  useLocation,
+} from "react-router-dom";
+import { useLayoutEffect } from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import App from "./App.tsx";
+import "./index.css";
+import Details from "./pages/Details.tsx";
 
+interface Props {
+  children: React.ReactNode;
+}
+
+const Wrapper = ({ children }: Props) => {
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  }, [location.pathname]);
+
+  return children;
+};
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <Router>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/home" element={<App />} />
-      <Route path="/details/:title" element={<Details />} />
-    </Routes>
+    <Wrapper>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/details/:title" element={<Details />} />
+      </Routes>
+    </Wrapper>
   </Router>
-)
+);
