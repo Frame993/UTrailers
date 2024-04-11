167d9053780772f412c6a32295376e2fd5b6710c
Yordani <yalvarezb93@gmail.com>
2024-04-11 13:13:35 +0300

    Add DetailsInfoMovies

---
+++ src/hooks/useDetailInfoMovie.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { movieDetails } from "../interfaces/movieDetails";
+import { Result } from "../interfaces/trendingAll";
+
+export const useDetailInfoMovie = (movie:Result)=>{
+    const apiURL = import.meta.env.VITE_MOVIES_API_URL;
+    const apiKey = import.meta.env.VITE_MOVIES_API_KEY;
+    const [details, setDetails ] = useState<movieDetails>()
+    const [isLoading, setIsLoading] = useState(false);
+
+    const getMovieDetails = async () => {
+        setIsLoading(true);
+        try {
+          const options = {
+            method: "GET",
+            headers: {
+              accept: "application/json",
+              Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOTM5MWZjNmVlZGJlM2E1OTdkMTJkZGM4ZGQ4ZDcwOCIsInN1YiI6IjY2MDQ1YjIyNDU5YWQ2MDE4N2ZkYjZmOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UMbylSXQaZYXv5swyjAhT2XwulJNIu_GM3tcD5CNE1k`
+            },
+          };
+    
+          const response = await fetch(
+            `${apiURL}${movie.title? 'movie' : 'tv'}/${movie.id}?language=en-US&page=1?api_key=${apiKey}`,
+            options
+          );
+          const data = await response.json();
+          return data;
+        } catch (e) {
+        } finally {
+          setIsLoading(false);
+        }
+      };
+
+
+    useEffect(()=>{
+        getMovieDetails().then((data)=>{
+            setDetails(data)
+        })
+    },[])
+
+    return {details, isLoading}
+}
+
--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,16 +1,17 @@
 import ButtonSlider from "../components/ButtonSlider";
 import prev from "../assets/Chevron left.svg";
-import ButtonWatch from "../components/ButtonWatch";
-import play_icon from "../assets/Play.png";
 
-import { Result } from "../interfaces/topRatedMovies";
+import { Result } from "../interfaces/trendingAll";
 import { useLocation } from "react-router-dom";
-
+import { useDetailInfoMovie } from "../hooks/useDetailInfoMovie";
 
 export default function Details() {
-
   const location = useLocation();
   const movie = location.state as Result;
+
+  const { details } = useDetailInfoMovie(movie);
+
+  console.log(details)
 
   return (
     <div className="w-[90%] md:w-[]1200px] mx-auto m-4">
@@ -24,17 +25,19 @@ export default function Details() {
         <section className="flex flex-col text-center items-center justify-end gap-4 h-[600px] overflow-hidden rounded-lg relative pb-16">
           <img
             className="w-full h-full object-cover absolute z-[-1]"
-            src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+            src={`https://image.tmdb.org/t/p/original${movie?.backdrop_path}`}
             alt="Poster"
           />
-          <h1 className="md:w-[50%] w-[90%] text-balance">{movie.title}</h1>
+          <h1 className="md:w-[50%] w-[90%] text-balance">
+            {movie?.title ?? movie.name}
+          </h1>
           <p className="text-white/60 w-[80%] mb-2">{movie.overview}</p>
-          <ButtonWatch text="Trailer" icon={play_icon} />
         </section>
         <section className="flex flex-col gap-4 bg-[#1A1A1A] p-8 rounded-lg">
-          <h3>
-            Realise year <span>2023</span>
-          </h3>
+          <div>
+            <img src="" alt="" />
+            <h3> Release year <span>{movie.release_date?.slice(0, 4) ?? movie.first_air_date?.slice(0, 4)}</span></h3>
+          </div>
           <h3>
             Available languages <span>English, hindi, etc...</span>
           </h3>
