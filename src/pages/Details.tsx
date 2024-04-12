9d05e14040df4ccd379dd58f6e985cac30104bcd
Yordani <yalvarezb93@gmail.com>
2024-04-12 11:16:59 +0300

    Add Details

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,17 +1,38 @@
 import ButtonSlider from "../components/ButtonSlider";
 import prev from "../assets/Chevron left.svg";
+import Icon_calendar from "../assets/Icon-calendar.png";
+import Icon_genres from "../assets/Icon-genres.png";
+import Icon_languages from "../assets/Icon_languages.png";
+import imdb from "../assets/imdb.png";
 
 import { TrendingAllResult } from "../interfaces/trendingAll";
 import { useLocation } from "react-router-dom";
 import { useDetailInfoMovie } from "../hooks/useDetailInfoMovie";
+import { useEffect, useState } from "react";
+import {
+  Genre,
+  ProductionCompany,
+  SpokenLanguage,
+} from "../interfaces/movieDetails";
+import DetailsDataSection from "../components/DetailsDataSection";
+import DetailsTag from "../components/DetailsTag";
 
 export default function Details() {
   const location = useLocation();
   const movie = location.state as TrendingAllResult;
 
   const { details } = useDetailInfoMovie(movie);
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [languages, setLanguages] = useState<SpokenLanguage[]>([]);
+  const [company, setCompany] = useState<ProductionCompany[]>([]);
 
-  console.log(details);
+  useEffect(() => {
+    if (details) {
+      setLanguages(details.spoken_languages);
+      setGenres(details.genres);
+      setCompany(details.production_companies);
+    }
+  });
 
   return (
     <div className="flex flex-col w-[90%] md:w-[]1200px] mx-auto m-4">
@@ -28,32 +49,52 @@ export default function Details() {
             src={`https://image.tmdb.org/t/p/original${movie?.backdrop_path}`}
             alt="Poster"
           />
-          <h1 className="md:w-[50%] w-[90%] text-balance">
+          <h1 className="md:w-[50%] w-[90%] text-balance text-[30px]">
             {movie?.title ?? movie.name}
           </h1>
+          <div className="critic-container flex flex-row gap-4">
+            <div className="imdb-container flex gap-2 items-center">
+              <img src={imdb} alt="imbd_icon" className="h-[18px]" />
+              <p>{`${details?.vote_average.toFixed(1)}`}</p>
+              <p className="text-white/80 text-[14px]">{`(${details?.vote_count} votes)`}</p>             
+            </div>
+          </div>
           <p className="text-white/60 w-[80%] mb-2">{movie.overview}</p>
         </section>
-        <section className="flex flex-col gap-4 bg-[#1A1A1A] p-8 rounded-lg">
-          <div>
-            <img src="" alt="" />
-            <h3>
-              {" "}
-              Release year{" "}
-              <span>
-                {movie.release_date?.slice(0, 4) ??
-                  movie.first_air_date?.slice(0, 4)}
-              </span>
-            </h3>
-          </div>
-          <h3>
-            Available languages <span>English, hindi, etc...</span>
-          </h3>
-          <h3>
-            Genres <span>Action, Fantasy</span>
-          </h3>
-          <h3>
-            IMDB <span>7.4</span>
-          </h3>
+        <section className="flex flex-col gap-4 bg-[#1A1A1A] p-10 rounded-lg">
+          <DetailsDataSection icon={Icon_calendar} text="Release Date">
+            <span className="text-white text-[20px]">
+              {movie.release_date?.slice(0, 4) ??
+                movie.first_air_date?.slice(0, 4)}
+            </span>
+          </DetailsDataSection>
+          <DetailsDataSection icon={Icon_languages} text="Available Languages">
+            <ul className="flex flex-row gap-2 flex-wrap ">
+              {languages.map((language) => (
+                <li key={language.english_name}>
+                  <DetailsTag name={language.english_name} />
+                </li>
+              ))}
+            </ul>
+          </DetailsDataSection>
+          <DetailsDataSection icon={Icon_genres} text="Genres">
+            <ul className="flex flex-row gap-2 flex-wrap ">
+              {genres.map((genre) => (
+                <li key={genre.id}>
+                  <DetailsTag name={genre.name} />
+                </li>
+              ))}
+            </ul>
+          </DetailsDataSection>
+          <DetailsDataSection text="Production Company">
+            <ul className="company-img flex flex-row flex-wrap gap-2">
+              {details?.production_companies.map((company) => (
+                <li key={company.id}>
+                  <DetailsTag name={company.name} />
+                </li>
+              ))}
+            </ul>
+          </DetailsDataSection>
         </section>
       </div>
     </div>
